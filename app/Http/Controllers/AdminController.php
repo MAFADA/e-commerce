50d92e172e1bc466e73acd9fa5c504a93ba53fb2
AFADA <afadanursaiva2001@gmail.com>
2021-11-30 15:44:58 +0700

    hapus bootstrap importan, edit admincontroller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,7 +13,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('user.admin.dashboard');
+        return view('admin.dashboard');
     }
 
     /**
