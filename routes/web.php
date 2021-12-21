b1fda45fb54891c21ec54954148f7ca14e859553
iqbalwaldan <iqbalwaldan0510@gmail.com>
2021-12-21 16:15:43 +0700

    Menu pembayaran

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\OrdersController;
 use App\Http\Controllers\Cust\EditProfileController;
 use App\Http\Controllers\Cust\ProfileController;
 use App\Http\Controllers\Cust\PaymentController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
