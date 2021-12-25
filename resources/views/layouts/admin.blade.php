cb89f119feb9cc8e6b17be0f20603c017548c708
AFADA <afadanursaiva2001@gmail.com>
2021-12-25 07:09:42 +0700

    tambah foto profile di sidebar admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -45,10 +45,10 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="" class="img-circle elevation-2" alt="User Image">
+          <img src="{{asset('storage/'.Auth::user()->photo)}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">{{ Auth::user()->username }}</a>
+          <a href="/profile" class="d-block">{{ Auth::user()->username }}</a>
         </div>
       </div>
 
